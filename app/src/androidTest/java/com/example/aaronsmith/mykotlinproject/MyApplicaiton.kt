a20867fb18f05543d2336acff550c39f6c348439
Aaron Smith <aaron_wolfx@163.com>
2017-12-24 23:58:40 +0800

    nothing

---
+++ app/src/androidTest/java/com/example/aaronsmith/mykotlinproject/MyApplicaiton.kt
@@ -0,0 +1,26 @@
+package com.example.aaronsmith.mykotlinproject
+
+import android.app.Application
+
+//  ┏┓　　　┏┓
+//┏┛┻━━━┛┻┓
+//┃　　　　　　　┃
+//┃　　　━　　　┃
+//┃　┳┛　┗┳　┃
+//┃　　　　　　　┃
+//┃　　　┻　　　┃
+//┃　　　　　　　┃
+//┗━┓　　　┏━┛
+//    ┃　　　┃   神兽保佑
+//    ┃　　　┃   代码无BUG！
+//    ┃　　　┗━━━┓
+//    ┃　　　　　　　┣┓
+//    ┃　　　　　　　┏┛
+//    ┗┓┓┏━┳┓┏┛
+//      ┃┫┫　┃┫┫
+//    
+/**
+ * Created by q on 2017/12/24.
+ */
+class MyApplicaiton : Application() {
+}
