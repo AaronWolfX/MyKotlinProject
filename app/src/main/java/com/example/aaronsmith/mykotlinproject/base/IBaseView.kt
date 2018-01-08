afbc64995fc9285ad2183addd3204b92bffe3678
Aaron Smith <aaron_wolfx@163.com>
2018-01-08 10:24:42 +0800

    Nothing

---
+++ app/src/main/java/com/example/aaronsmith/mykotlinproject/base/IBaseView.kt
@@ -0,0 +1,28 @@
+package com.example.aaronsmith.mykotlinproject.base
+
+//  ┏┓　　　┏┓
+//┏┛┻━━━┛┻┓
+//┃　　　　　　　┃
+//┃　　　━　　　┃
+//┃　┳┛　┗┳　┃
+//┃　　　　　　　┃
+//┃　　　┻　　　┃
+//┃　　　　　　　┃
+//┗━┓　　　┏━┛
+//    ┃　　　┃   神兽保佑
+//    ┃　　　┃   代码无BUG！
+//    ┃　　　┗━━━┓
+//    ┃　　　　　　　┣┓
+//    ┃　　　　　　　┏┛
+//    ┗┓┓┏━┳┓┏┛
+//      ┃┫┫　┃┫┫
+// 
+/**
+ * Created by aaronsmith on 29/12/2017.
+ */
+interface IBaseView {
+
+    fun showLoading()
+
+    fun dismissLoading()
+}
---
+++ app/src/main/java/com/example/aaronsmith/mykotlinproject/utils/NetworkUtil.kt
@@ -0,0 +1,71 @@
+package com.example.aaronsmith.mykotlinproject.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import java.net.NetworkInterface
+import java.net.SocketException
+
+//  ┏┓　　　┏┓
+//┏┛┻━━━┛┻┓
+//┃　　　　　　　┃
+//┃　　　━　　　┃
+//┃　┳┛　┗┳　┃
+//┃　　　　　　　┃
+//┃　　　┻　　　┃
+//┃　　　　　　　┃
+//┗━┓　　　┏━┛
+//    ┃　　　┃   神兽保佑
+//    ┃　　　┃   代码无BUG！
+//    ┃　　　┗━━━┓
+//    ┃　　　　　　　┣┓
+//    ┃　　　　　　　┏┛
+//    ┗┓┓┏━┳┓┏┛
+//      ┃┫┫　┃┫┫
+// 
+/**
+ * Created by aaronsmith on 29/12/2017.
+ */
+class NetworkUtil {
+
+    companion object {
+        var NET_CNNT_BAIDU_OK = 1 // NetworkAvailable
+        var NET_CNNT_BAIDU_TIMEOUT = 2 // no NetworkAvailable
+        var NET_NOT_PREPARE = 3 // Net no ready
+        var NET_ERROR = 4 //net error
+        private val TIMEOUT = 3000 // TIMEOUT
+
+
+        /**
+         * check NetworkAvailable
+         *
+         * @param context
+         * @return
+         */
+        @JvmStatic
+        fun isNetworkAvailable(context: Context): Boolean {
+            val manager = context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val info = manager.activeNetworkInfo
+            return !(null == info || !info.isAvailable)
+        }
+
+        @JvmStatic
+        fun getLocalIpAddress(): String {
+            var ret = ""
+            try {
+                val en = NetworkInterface.getNetworkInterfaces()
+                while (en.hasMoreElements()) {
+                    val enumIpAddress = en.nextElement().inetAddresses
+                    while (enumIpAddress.hasMoreElements()) {
+                        val netAddress = enumIpAddress.nextElement()
+                        if (!netAddress.isLoopbackAddress) {
+                            ret = netAddress.hostAddress.toString()
+                        }
+                    }
+                }
+            } catch (ex: SocketException) {
+                ex.printStackTrace()
+            }
+            return ret
+        }
+    }
+}
