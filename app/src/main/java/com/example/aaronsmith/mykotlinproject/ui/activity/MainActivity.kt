dde570f288ccc641ce83378a061d79ec18c3b07b
AaronSmith <aaronsmithyx@gmail.com>
2018-02-05 16:17:49 +0800

    测试git二分法——第一次提交

--- app/src/main/java/com/example/aaronsmith/mykotlinproject/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/aaronsmith/mykotlinproject/ui/activity/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.aaronsmith.mykotlinproject.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.aaronsmith.mykotlinproject.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text.setText("第一次提交")
     }
 }
